5e02909f828157f166c3090702c0c65cd04027fe
dongcheon1020 <dongcheon1020@gmail.com>
2024-04-02 22:51:09 +0900

    fix

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -9,7 +9,7 @@ function getProducts(searchQuery) {
 
 function getProductDetail(id) {
   return async (dispatch) => {
-    let url = `http://my-json-server.typicode.com/dongcheon1020/H-M-page/products/${id}`;
+    let url = `https://my-json-server.typicode.com/dongcheon1020/H-M-page/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     dispatch({
